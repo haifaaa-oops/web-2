dbc436d991ff17546a1524f06b083da67b3f1d48
haifaaa-oops <haifaamr03@gmail.com>
2025-02-21 10:25:08 +0700

    praktikum 1

--- README.md
+++
@@ -1,6 +0,0 @@
-# Pemrograman Web 2
-
-## Repisitori Praktikum Pemrograman Web 2
-- Nama : Haifa Muzdalifah Ramadhani
-- NIM :0110124164
-- Rombel : SI 11
---
+++ variabel_user.php
@@ -0,0 +1,11 @@
+<?php
+// mendefinisikan variable
+$nama = 'Ipet';
+$umur = 18;
+$berat = 53.0;
+
+echo 'Nama Saya ' . $nama;
+echo '<br/>Umur ' . $umur . ' Tahun';
+echo '<br/>Berat ' . $berat;
+
+?>
